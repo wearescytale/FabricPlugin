39df2a92dd31bf39b0dfeb8fa9d8ffe42d22b8c9
Nelson Brandão <nelsonbrandao800@gmail.com>
2017-02-09 16:14:39 +0000

    Added debug messages

--- hooks/after_platform_add.js
+++ hooks/after_platform_add.js
@@ -4,7 +4,7 @@ var iosHelper = require("./lib/ios-helper");
 var utilities = require("./lib/utilities");
 
 module.exports = function(context) {
-
+    console.log("FabricPlugin: After Platform Add");
     var platforms = context.opts.cordova.platforms;
 
     // Modify the Gradle build file to add a task that will upload the debug symbols
@@ -17,6 +17,7 @@ module.exports = function(context) {
     // Add a build phase which runs a shell script that executes the Crashlytics
     // run command line tool which uploads the debug symbols at build time.
     if (platforms.indexOf("ios") !== -1) {
+        console.log("FabricPlugin: After Platform Add - IOS");
         var xcodeProjectPath = utilities.getXcodeProjectPath(context);
         iosHelper.removeShellScriptBuildPhase(context, xcodeProjectPath);
         iosHelper.addShellScriptBuildPhase(context, xcodeProjectPath);
